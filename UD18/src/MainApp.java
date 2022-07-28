b6a1c6ec97a1b1b885a80f0d1cf7b1fa2ea7be79
David Marcos <david.13254@gmail.com>
2022-07-28 09:42:27 +0200

    createTable()

--- UD18/src/MainApp.java
+++ UD18/src/MainApp.java
@@ -1,9 +1,29 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainApp {
+	
+	static Connection connection;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private static void createTable(String database, String table, String columns) {
+		try {
+			String queryUseDb = "USE "+database+";";
+			Statement statementDb = connection.createStatement();
+			statementDb.executeUpdate(queryUseDb);
+			
+			String queryCreateTable = "CREATE TABLE "+table+" ("+columns+");";
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(queryCreateTable);
+			System.out.println("Table created succesfully");
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
 }
